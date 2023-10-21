20f29fbcea312e8586bc7588371eac1ba77a37d5
Ersin Mandacı <ersinmandaci0@gmail.com>
2023-10-21 14:31:37 +0300

    try-catch added

--- lib/core/services/product/product_service.dart
+++ lib/core/services/product/product_service.dart
@@ -5,10 +5,23 @@ import 'package:groceries_app/core/constants/string.dart';
 import 'package:groceries_app/model/groceries.dart';
 
 class ProductService {
-  Future<Groceries> loadJson() async {
-    final jsonString = await rootBundle.loadString(StringConst.jsonProduct);
-    final jsonData = jsonDecode(jsonString) as Map<String, dynamic>;
-    final groceries = Groceries.fromJson(jsonData);
-    return groceries;
+  Future<Groceries?> loadJson() async {
+    try {
+      final jsonString = await rootBundle.loadString(StringConst.jsonProduct);
+      final jsonData = jsonDecode(jsonString);
+
+      if (jsonData is! Map<String, dynamic>) {
+        throw FormatException('Unexpected JSON structure');
+      }
+
+      final groceries = Groceries.fromJson(jsonData);
+      return groceries;
+    } on FormatException catch (e) {
+      throw Exception('Format Exception: $e');
+    } on PlatformException catch (e) {
+      throw Exception('Platform Exception: $e');
+    } catch (e) {
+      throw Exception('Exception: $e');
+    }
   }
 }
