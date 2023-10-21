e42f4bb78b81aa9d7d91222bf4ea1886718162d6
Ersin Mandacı <ersinmandaci0@gmail.com>
2023-10-21 22:33:31 +0300

    comment lines added

--- lib/core/services/firestore/firestore_service.dart
+++ lib/core/services/firestore/firestore_service.dart
@@ -5,27 +5,32 @@ import 'package:groceries_app/model/orders.dart';
 import 'package:groceries_app/model/products.dart';
 import 'package:groceries_app/model/user_model.dart';
 
-class FirestoreService {
+final class FirestoreService {
   final FirestoreRepository _firestoreRepository =
       LocatorManager.firestoreRepository;
 
+
+  // add user to firestore
   Future<bool> addUser(UserModel user) {
     if (user.userID == null) {
       return Future.value(false);
     }
     return _firestoreRepository.addUser(user);
   }
-
+  // add product to firestore
   Future<List<Orders>> getOrders() async {
     final ordersCollection = _firestoreRepository.getOrders();
 
     return ordersCollection;
   }
 
+
+  // add product to firestore
   Future<void> pushBasketDataToFirestore(List<Products> basketProducts) {
     return _firestoreRepository.pushBasketDataToFirestore(basketProducts);
   }
 
+  // read user from firestore
   Future<UserModel> readUser(String userID) {
     if (userID.isEmpty) {
       return Future.value(UserModel());
